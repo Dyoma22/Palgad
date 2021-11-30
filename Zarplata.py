2d0dd80fcf86e1d155637622ca7dc1db703572b2
opilane <opilane@HP-CZC6297B9B>
2021-11-30 14:03:13 +0200

    dw

--- Zarplata.py
+++ Zarplata.py
@@ -2,4 +2,10 @@ from palga import*
 palgad=[1200,2500,750,395,1200]
 inimesed=["A","B","C","D","E"]
 
-
+while True:
+    print("Lisamine-1,Otsi inimest-7")
+    valik = input("Millist valite->")
+    if valik == "1":
+        i,p = lisamine(inimesed,palk)
+    elif valik =="7":
+        inimesed,palk=otsing_nimi_jargi(inimesed,palgad)
--- palga.py
+++ palga.py
@@ -1,1 +1,29 @@
-
+   
+def lisamine(i,p): 
+    """
+    i-inimeste nimikiri,p-palka nimikiri 
+    """
+    kogus = int(input("Lisa inimest: "))
+    for j in range(kogus):
+        nimi = input("Nimi: ")
+        i.append(nimi)
+        palgad = int(input("Palk: "))
+        p.append(palk)
+        return i,p
+def otsing_nimi_jargi(inimesed:list,palgad:list):
+    """Tagastame jäejend või tekst
+    :rtype var: tulemus
+    """
+    nimi=input("Keda otsime?")
+    for inimene in inimesed:
+        if inimene==nimi:
+            n=inimesed.count(nimi)
+            print("Inimene on olemas,selline kohtume",n,"korda")
+            b=-1
+            for i in range(n):
+                k=inimesed.index(nimi,b)
+                palk=palgad[k]
+                print(nimi,palk)
+        else:
+             t="Ei ole nimekirjas"
+    return t
